f631c36efa480b69380fe87c109cc3a456677fd9
Da Liu <dluoft@gmail.com>
2023-07-11 00:16:58 -0400

    update title, make sure links work

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,8 @@ function App() {
     <>
       <h1>Hello</h1>
       <div>
-        <p><a href="https://delay.daliu.ca">delay</a></p>
-        <p><a href="https://gcal.daliu.ca">gcal</a></p>
-        <p><a href="https://ama.daliu.ca">ama</a></p>
+        <p><a href="https://ama-sb.surge.sh">ama</a></p>
+        <p><a href="https://gcal.surge.sh">cal</a></p>
       </div>
     </>
   )
